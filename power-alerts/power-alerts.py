6ee5b2a06de5f28b54124c47f9c7abcf08e4759c
dmatech1 <36796590+dmatech1@users.noreply.github.com>
2023-09-25 09:13:38 -0400

    #2. Improved the embed formatting slightly.

--- power-alerts/power-alerts.py
+++ power-alerts/power-alerts.py
@@ -6,8 +6,33 @@ import typing
 import time
 import requests
 import os
+import netutils
 
 _re_var_value = re.compile(R'VAR ([^ ]+) ([^ ]+) "(.*)"')
+
+# See: https://github.com/networkupstools/nut/blob/master/docs/new-drivers.txt#L223-L238
+_status_desc = {
+    "OL":       "Online",
+    "OB":       "On Battery",
+    "LB":       "Low Battery",
+    "HB":       "High Battery",
+    "RB":       "Replace Battery",
+    "CHRG":     "Charging",
+    "DISCHRG":  "Discharging",
+    "BYPASS":   "Bypassed",
+    "CAL":      "Calibrating",
+    "OFF":      "Offline",
+    "OVER":     "Overloaded",
+    "TRIM":     "Trimming",
+    "BOOST":    "Boosting",
+    "FSD":      "Forced Shutdown"
+}
+
+def describe_ups_status(ups_status: typing.Optional[str]) -> str:
+    if isinstance(ups_status, str):
+        return ", ".join(_status_desc.get(x, x) for x in ups_status.split())
+    else:
+        return "Unknown"
 
 class NutNetClient:
     """
@@ -43,7 +68,6 @@ class NutNetClient:
         
         return ret
 
-
 # Allow for the user to specify the webhook URL in a script or interactively.
 DISCORD_WEBHOOK = os.getenv('DISCORD_WEBHOOK')
 DISCORD_RECIPIENT = os.getenv('DISCORD_RECIPIENT')
@@ -60,13 +84,17 @@ interesting_fields = [
 ]
 
 nnc = NutNetClient(("127.0.0.1", 3493))
-
+ups_name = "apc-1"
 ups_status : typing.Optional[str] = "Unknown"
 
+rs = requests.Session()
+rs.mount("https://", netutils.AlternateInterfaceAdapter("usb0"))
+
 while True:
-    ups_vars = nnc.list_vars("apc-1")
+    ups_vars = nnc.list_vars(ups_name)
     new_ups_status = ups_vars.get("ups.status", "")
 
+    # Set the embed color based on the status.
     if new_ups_status == "OL":
         color = 0x00FF00
     elif new_ups_status == "OB DISCHRG":
@@ -76,17 +104,21 @@ while True:
     
     if ups_status != new_ups_status:
         msg = {
-            "content": DISCORD_RECIPIENT + "UPS Status: `" + ups_status + "` :arrow_right: `" + new_ups_status + "`",
+            "content": ups_name + ": " + describe_ups_status(new_ups_status) + " \u2B05\uFE0F " + describe_ups_status(ups_status) + " - " + DISCORD_RECIPIENT,
             "embeds": [
                 {
-                    "title": "Status Change: apc-1",
+                    "title": "Status Change: " + ups_name,
                     "color": color,
-                    "fields": [{"name": k, "inline": True, "value": ups_vars.get(k, "")} for k in interesting_fields]
+                    "fields": [{"name": k, "inline": True, "value": ups_vars.get(k, "")} for k in interesting_fields],
+                    "thumbnail": {
+                        # Taken from https://download.schneider-electric.com/files?p_Doc_Ref=SPD_MMAE-7UCQFM_FL_V&p_File_Type=rendition_369_jpg&default_image=DefaultProductImage.png
+                        "url": "https://cdn.discordapp.com/attachments/768106411765923878/1155262743931265084/SPD_MMAE-7UCQFM_FL_V_web.JPG"
+                    }
                 }
             ]
         }
 
-        r = req = requests.post(DISCORD_WEBHOOK, json=msg)
+        r = req = rs.post(DISCORD_WEBHOOK, json=msg)
         
         # Notify.
         pass
